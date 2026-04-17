3dbf0e268bc2ef8bc47da67d8388da7443c5ee98
Faruk-ahmed702 <shiledarfaruk@gmail.com>
2026-04-17 17:27:49 +0530

    Amazon front-end project

---
+++ javaScript.js
@@ -0,0 +1,14 @@
+﻿function searchProduct() {
+    let input = document.getElementById("searchBox").value.toLowerCase();
+    let cards = document.querySelectorAll(".card");
+
+    cards.forEach(card => {
+        let title = card.querySelector("h3").innerText.toLowerCase();
+
+        if (title.includes(input)) {
+            card.style.display = "block";
+        } else {
+            card.style.display = "none";
+        }
+    });
+}
